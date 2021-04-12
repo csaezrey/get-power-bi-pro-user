59952466f6785ef8022051407a5cead7f67e2bc0
Camilo Saez Reyes <camilo.saez@sfg.cl>
2021-04-11 22:15:00 -0400

    Versión inicial del código

---
+++ Get-PowerBIProUser.ps1
@@ -0,0 +1,26 @@
+﻿#Credenciales de acceso
+$user = "user@domain.com"
+$password = ConvertTo-SecureString "password" -AsPlainText -Force  
+$credential = New-Object System.Management.Automation.PSCredential ($user, $password)
+Connect-MsolService -Credential $credential
+
+#Obtención de licencias de usuario y filtrado por Power BI Pro
+$users=(Get-MsolUser -EnabledFilter EnabledOnly -MaxResults 2000 | Where-Object {$_.licenses.AccountSkuId -like "*POWER_BI_PRO*"})
+
+#Variables
+$path = "C:\path\"
+$file="file.csv"
+$filepath=$path+$file
+$delimited = ";"
+$head="`"UserPrincipalName`""
+
+#Setea la cabecera del CSV
+Set-Content -Path  $filepath -Value $head
+
+#Itera los usuarios y los almacena en CSV
+Foreach ($user in $users)
+{	
+	$record = $user.UserPrincipalName		
+	Add-Content -Path $filepath  -Value $record
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# get-power-bi-pro-user
+Fragmento de código que permite obtener los usuarios Power BI con licencia Pro. Para funcionar las credenciales ser de un usuario administrador en Power BI.
